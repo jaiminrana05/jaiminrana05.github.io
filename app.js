475981fb42ddd08c26e158b8e163b28c9b6c1523
jaiminrana05 <96230379+jaiminrana05@users.noreply.github.com>
2022-11-24 12:08:50 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,5 @@
+ function changeSplashScreen(){
+    console.log("Executed");
+        var element = document.getElementById("center-con");
+        element.innerHTML = '<div class="lds-roller" style="position:absolute;bottom: 50%"><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div></div>';
+    }
